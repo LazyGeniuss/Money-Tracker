7a8f1ac6c53d36c679ea94ffe9895c3895433d86
Shreyash <shreyash@gmail.com>
2023-05-30 21:36:02 +0530

    fixed mongodb error

--- api/index.js
+++ api/index.js
@@ -9,19 +9,19 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get("/api/test", (req,res) => {
     res.json({body : "test oks"})
 })
 
 app.post("/api/transaction", async (req,res) => {
-    await mongoose.connect(process.env.MONGO_URL);
     const {name, description, datetime, price} = req.body;
     const transaction = await Transaction.create({name, description, datetime, price});
     return res.json(transaction);
 })
 
 app.get("/api/transactions", async (req, res) => {
-    await mongoose.connect(process.env.MONGO_URL);
     const transactions = await Transaction.find();
     res.json(transactions);
 
